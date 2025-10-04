d809eba887facdf9555c88c6339f23cff4377913
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-04 14:29:11 +0700

    first-commit

---
+++ src/main/java/sum/batch/springbatchusecase/SpringBatchUseCaseApplication.java
@@ -0,0 +1,13 @@
+package sum.batch.springbatchusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUseCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchUseCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sum/batch/springbatchusecase/SpringBatchUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package sum.batch.springbatchusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchUseCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
