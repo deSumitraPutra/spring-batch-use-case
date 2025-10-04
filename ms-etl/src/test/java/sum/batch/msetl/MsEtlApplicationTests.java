97ed6f8004b22a62a710d32d39a15b85a4ded676
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-04 17:13:26 +0700

    feat(basic-migration): add default test

---
+++ ms-etl/src/test/java/sum/batch/msetl/MsEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package sum.batch.msetl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
