da95eb9c1592d710142f852fc7503f2d9fbbac78
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-04 17:27:28 +0700

    feat(basic-migration): add component scan

--- ms-etl/src/main/java/sum/batch/msetl/MsEtlApplication.java
+++ ms-etl/src/main/java/sum/batch/msetl/MsEtlApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan({"sum.batch.etlcore.*"})
-@EnableJpaRepositories({"sum.batch.etlcore.*"})
+@EntityScan({"sum.batch.*"})
+@ComponentScan({"sum.batch.*"})
+@EnableJpaRepositories({"sum.batch.*"})
 public class MsEtlApplication {
 
     public static void main(String[] args) {
