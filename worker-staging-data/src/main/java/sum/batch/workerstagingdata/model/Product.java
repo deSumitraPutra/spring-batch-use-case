15b35aba3b33b89fd0882ddef24f7ec653d32aed
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-12 17:29:14 +0700

    feat(basic-migration): add entity model

---
+++ worker-staging-data/src/main/java/sum/batch/workerstagingdata/model/Product.java
@@ -0,0 +1,34 @@
+package sum.batch.workerstagingdata.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Date createdAt;
+
+    public static Product fromResultSet(ResultSet rs) throws SQLException {
+        return Product.builder()
+            .code(rs.getString("CODE"))
+            .name(rs.getString("NAME"))
+            .description(rs.getString("DESCRIPTION"))
+            .price(rs.getBigDecimal("PRICE"))
+            .createdAt(rs.getDate("CREATED_AT"))
+            .build();
+    }
+}
