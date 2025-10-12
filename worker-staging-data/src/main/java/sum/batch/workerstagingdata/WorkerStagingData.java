efe0ee4fbc9b5d70ea4f0171b88fce829a128d10
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-12 17:32:42 +0700

    feat(basic-migration): setup application properties

---
+++ worker-staging-data/src/main/java/sum/batch/workerstagingdata/WorkerStagingData.java
@@ -0,0 +1,11 @@
+package sum.batch.workerstagingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerStagingData {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerStagingData.class, args);
+    }
+}
