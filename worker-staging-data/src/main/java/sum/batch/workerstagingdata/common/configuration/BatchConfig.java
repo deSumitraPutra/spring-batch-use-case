8d3be884bebd94212eed1f4ef4e884aaeb2c2ad2
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-10-12 17:27:31 +0700

    feat(basic-migration): add async task executor for job launcher

--- worker-staging-data/src/main/java/sum/batch/workerstagingdata/common/configuration/BatchConfig.java
+++ worker-staging-data/src/main/java/sum/batch/workerstagingdata/common/configuration/BatchConfig.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -20,6 +21,10 @@ import javax.sql.DataSource;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
+    /**
+     * Setting H2 as Spring Batch Metadata storage is not recommended because the data is in-memory
+     * which can be disappeared when service restart
+     */
     @Bean
     public JobRepository jobRepository(
         @Qualifier("dataSource") DataSource dataSource,
@@ -34,6 +39,11 @@ public class BatchConfig {
         return factory.getObject();
     }
 
+    /**
+     * JobLauncher set to use ThreadPoolTaskExecutor because multiple kafka message that arrive at almost
+     * same time can trigger the multiple job using the same Threads.
+     * By default, Spring Batch JobLauncher executed at synchronously.
+     */
     @Bean
     public JobLauncher jobLauncher(@Qualifier("jobRepository") JobRepository jobRepository) throws Exception {
         TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
@@ -44,6 +54,35 @@ public class BatchConfig {
         return jobLauncher;
     }
 
+    /**
+     * JobLauncher set to use ThreadPoolTaskExecutor because multiple kafka message that arrive at almost
+     * same time can trigger the multiple job using the same Threads.
+     * By default, Spring Batch JobLauncher executed at synchronously.
+     */
+    @Bean
+    public JobLauncher asyncJobLauncher(@Qualifier("jobRepository") JobRepository jobRepository) throws Exception {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4); // minimum always alive thread
+        executor.setMaxPoolSize(8); // max thread for bursts
+        executor.setQueueCapacity(16); // how many pending jobs before blocking
+        executor.setWaitForTasksToCompleteOnShutdown(true); // graceful stop on shutdown
+        executor.setAwaitTerminationSeconds(60); // wait before force kill
+        executor.setThreadNamePrefix("batch-job-"); // for log clarity
+        executor.initialize();
+
+        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(executor);
+        jobLauncher.afterPropertiesSet();
+
+        return jobLauncher;
+    }
+
+    /**
+     * This initializer is not recommended for production-grade service. Because, this part is executed
+     * everytime the service is started. That can cause error if not configured correctly. It may cause
+     * duplicate table creation, etc.
+     */
     @Bean
     public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") DataSource dataSource) {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
