765835e6f90cd85acf178f6bb03a6ba5250b774c
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-11-19 13:50:34 +0800

    feat(multi-db-transformation): configure kafka producer and consumer

---
+++ worker-transformation/src/main/java/sum/batch/workertransformation/common/configuration/KafkaConfig.java
@@ -0,0 +1,54 @@
+package sum.batch.workertransformation.common.configuration;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+    @Value("${kafka.bootstrap.servers}")
+    private String bootstrapServers;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(this.consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    ConsumerFactory<String, String> consumerFactory() {
+        HashMap<String, Object> consumerProps = new HashMap<>();
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "transformation_job");
+
+        return new DefaultKafkaConsumerFactory<>(consumerProps);
+    }
+
+    @Bean
+    ProducerFactory<String, String> producerFactory() {
+        HashMap<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(this.producerFactory());
+    }
+}
