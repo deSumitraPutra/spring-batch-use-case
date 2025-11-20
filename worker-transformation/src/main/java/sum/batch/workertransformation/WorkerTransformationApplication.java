5cc72c6b7f35c4cab181318c414ae1db5807f04f
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-11-20 12:31:59 +0800

    feat(multi-db-transformation): main application

---
+++ worker-transformation/src/main/java/sum/batch/workertransformation/WorkerTransformationApplication.java
@@ -0,0 +1,13 @@
+package sum.batch.workertransformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerTransformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerTransformationApplication.class, args);
+    }
+
+}
---
+++ worker-transformation/src/test/java/sum/batch/workertransformation/WorkerTransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package sum.batch.workertransformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerTransformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
