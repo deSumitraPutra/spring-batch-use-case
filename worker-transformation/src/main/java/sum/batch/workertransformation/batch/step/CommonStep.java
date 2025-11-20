c783a54391324a3ed9059d2ecd83185a0a88edd9
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-11-20 12:29:36 +0800

    feat(multi-db-transformation): add common step

---
+++ worker-transformation/src/main/java/sum/batch/workertransformation/batch/step/CommonStep.java
@@ -0,0 +1,26 @@
+package sum.batch.workertransformation.batch.step;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import sum.batch.workertransformation.service.EtlItemService;
+
+@Configuration
+@RequiredArgsConstructor
+public class CommonStep {
+    private final EtlItemService etlItemService;
+
+    public Step markAsRunningStep(JobRepository jobRepository, PlatformTransactionManager transactionManager, String jobName) {
+        String stepName = "MARK_AS_RUNNING -> " + jobName;
+        return new StepBuilder(stepName, jobRepository)
+            .tasklet((contribution , chunkContext) -> {
+                this.etlItemService.updateToRunning(jobName);
+                return RepeatStatus.FINISHED;
+            }, transactionManager)
+            .build();
+    }
+}
