2e968d159bead1e4756232905a1fa35277abc143
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-11-20 12:31:38 +0800

    feat(multi-db-transformation): add etl item service and notification service

---
+++ worker-transformation/src/main/java/sum/batch/workertransformation/service/EtlItemService.java
@@ -0,0 +1,48 @@
+package sum.batch.workertransformation.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import static sum.batch.workertransformation.common.constant.MainDB.EtlItemQuery.*;
+
+@Repository
+@RequiredArgsConstructor
+public class EtlItemService {
+    private final JdbcTemplate mainJdbcTemplate;
+
+    @Transactional(transactionManager = "mainTransactionManager")
+    public void updateToRunning(String jobName) {
+        int affectedRow = this.mainJdbcTemplate.update(MARK_AS_RUNNING, jobName);
+        this.validateResult(jobName, affectedRow);
+    }
+
+    @Transactional(transactionManager = "mainTransactionManager")
+    public void finalized(String status, String message, String jobName) {
+        int affectedRow = this.mainJdbcTemplate.update(MARK_FINAL, status, message, jobName);
+        this.validateResult(jobName, affectedRow);
+    }
+
+    private void validateResult(String jobName, int affectedRow) {
+        if (affectedRow > 1) {
+            String errorMessage = "Etl Item with job name: " + jobName + " has multiple row in database!";
+            throw new IllegalArgumentException(errorMessage);
+        }
+        if (affectedRow == 0) {
+            String errorMessage = "Etl Item with job name: " + jobName + " not found!";
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public void createHistory(String jobName) {
+        this.mainJdbcTemplate.execute(
+            INSERT_HISTORY,
+            (PreparedStatementCallback<Boolean>) ps -> {
+                ps.setString(1, jobName);
+                return ps.execute();
+            }
+        );
+    }
+}
---
+++ worker-transformation/src/main/java/sum/batch/workertransformation/service/NotificationService.java
@@ -0,0 +1,19 @@
+package sum.batch.workertransformation.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka.notification.topic}")
+    private String topic;
+
+    public void sendNotification(String message) {
+        this.kafkaTemplate.send(topic, message);
+    }
+}
